19fa34763f7f6830ac767bfa7e38229ef9bfca03
Marti <44981000+marti-toonen@users.noreply.github.com>
2024-05-04 19:42:28 -0400

    2nd commit

--- site.js
+++ site.js
@@ -2,12 +2,17 @@ let name = document.getElementById('name');
 let birthday = document.getElementById('birthday');
 let job = document.getElementById('occupation');
 let death = document.getElementById('deceased');
+let partner = document.getElementById('partner');
+let children = document.getElementById('children');
+let parents = document.getElementById('parents');
 
 function toggleRow(generation) {
-  if(document.getElementById(generation).style.display == "flex")
+  if(document.getElementById(generation).style.display == "flex") {
     document.getElementById(generation).style.display = "none";
-  else
-    document.getElementById(generation).style.display = "flex"; }
+    document.getElementById('tooltip').style.display = "none"; }
+  else {
+    document.getElementById(generation).style.display = "flex";
+    generation_menu_open = true; }}
 
 function toggleMenu(id) {
   if(document.getElementById('tooltip').style.display == "flex") {
@@ -16,59 +21,96 @@ function toggleMenu(id) {
   else { document.getElementById('tooltip').style.display = "flex"; last_id = id; }
 
    switch(id) {
-    case 'mrt':
-      name.innerHTML = "John Toonen";
-      birthday.innerHTML = "April 27th 1934";
-      job.innerHTML = "Farmer";
-      break;
-    case 'cory':
-      name.innerHTML = "Cory Toonen";
-      birthday.innerHTML = "";
-      death.innerHTML = " - ";
+     case 'hermanus':
+      name.innerHTML = "Hermanus Toonen";
+      birthday.innerHTML = "???";
+      death.innerHTML = " - August 24th, 1807";
+      parents.innerHTML = "Parents: ???";
+      partner.innerHTML = "Partner: Catherine Peters Toonen (née Arts)";
+      children.innerHTML = "Children: Antonia, Antonius, Petrus, Joannes, Joannes, Joanna";
       job.innerHTML = "";
       break;
-    case 'bernadine':
-      name.innerHTML = "Bernadine";
-      birthday.innerHTML = "";
-      death.innerHTML = " - ";
+    case 'catherine':
+      name.innerHTML = "Catherine Peters Toonen (née Arts)";
+      birthday.innerHTML = "February 9th, 1741";
+      death.innerHTML = " - November 9th, 1825";
+      parents.innerHTML = "Parents: ???";
+      partner.innerHTML = "Partner: Hermanus Toonen";
+      children.innerHTML = "Children: Antonia, Antonius, Petrus, Joannes, Joannes, Joanna";
       job.innerHTML = "";
       break;
-    case 'charlie':
-      name.innerHTML = "Charles Underhill";
-      birthday.innerHTML = "1915";
-      death.innerHTML = "";
-      job.innerHTML = "Firewatch";
+    case 'antonia1':
+      name.innerHTML = "Antonia Toonen";
+      birthday.innerHTML = "May 10th, 1765";
+      death.innerHTML = " - ???";
+      parents.innerHTML = "Parents: Hermanus & Catherine";
+      partner.innerHTML = "";
+      children.innerHTML = "";
+      job.innerHTML = "";
       break;
-    case 'kathi':
-      name.innerHTML = "Kathi Lynn Underhill";
-      birthday.innerHTML = "August 4th 1961";
-      job.innerHTML = "Model";
+    case 'antonius':
+      name.innerHTML = "Antonius Toonen";
+      birthday.innerHTML = "July 8th, 1766";
+      death.innerHTML = " - ???";
+      parents.innerHTML = "Parents: Hermanus & Catherine";
+      partner.innerHTML = "";
+      children.innerHTML = "";
+      job.innerHTML = "";
       break;
-    case 'andy':
-      name.innerHTML = "Anthony Peter Toonen";
-      birthday.innerHTML = "February 5th 1961";
-      job.innerHTML = "Farmer";
+    case 'petrus':
+      name.innerHTML = "Petrus Toonen";
+      birthday.innerHTML = "August 14th, 1767";
+      death.innerHTML = " - June 9th, 1852";
+      parents.innerHTML = "Parents: Hermanus & Catherine";
+      partner.innerHTML = "Partner: Hendrina Mathijs Toonen (née Hendriks)";
+      children.innerHTML = "Children: Hermina, Mathias, Gertruy, Jan, Elisabeth, Peter, Hendricus, Arnoldus, Petronella";
+      job.innerHTML = "";
       break;
-    case 'marti':
-      name.innerHTML = "Marti Anne Toonen";
-      birthday.innerHTML = "September 4th 1995";
-      job.innerHTML = "Game Developer";
+    case 'hendrina':
+      name.innerHTML = "Hendrina Mathijs Toonen (née Hendriks)";
+      birthday.innerHTML = "August 14th, 1767";
+      death.innerHTML = " - June 9th, 1852";
+      parents.innerHTML = "Parents: ???";
+      partner.innerHTML = "Partner: Petrus Toonen";
+      children.innerHTML = "Children: Hermina, Mathias, Gertruy, Jan, Elisabeth, Peter, Hendricus, Arnoldus, Petronella";
+      job.innerHTML = "";
       break;
-    case 'annie':
-      name.innerHTML = "Annie Lynn Toonen";
-      birthday.innerHTML = "December 4th 1999";
-      job.innerHTML = "Behavioural Therapist, right?";
-      break;
-    case 'brock':
-      name.innerHTML = "Brock Morgan Toonen";
-      birthday.innerHTML = "November 5th 1986";
-      job.innerHTML = "Something Cars";
-      break;
-    case 'alissa':
-      name.innerHTML = "Alissa Wilson";
-      birthday.innerHTML = "Something, I'm Sure";
-      job.innerHTML = "Who Knows";
-      break;
+      case 'antonia2':
+        name.innerHTML = "Antonia Toonen";
+        birthday.innerHTML = "March 15th, 1769";
+        death.innerHTML = " - ???";
+        parents.innerHTML = "Parents: Hermanus & Catherine";
+        partner.innerHTML = "";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'joannes1':
+        name.innerHTML = "Joannes Toonen";
+        birthday.innerHTML = "February 19th, 1771";
+        death.innerHTML = " - February 28th, 1771";
+        parents.innerHTML = "Parents: Hermanus & Catherine";
+        partner.innerHTML = "";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'joannes2':
+        name.innerHTML = "Joannes Toonen";
+        birthday.innerHTML = "March 28th, 1772";
+        death.innerHTML = " - ???";
+        parents.innerHTML = "Parents: Hermanus & Catherine";
+        partner.innerHTML = "";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'joanna':
+        name.innerHTML = "Joanna Toonen";
+        birthday.innerHTML = "June 25th, 1773";
+        death.innerHTML = " - ???";
+        parents.innerHTML = "Parents: Hermanus & Catherine";
+        partner.innerHTML = "";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
     case null:
       alert('tell marti it said null and who you clicked on');
       break;
