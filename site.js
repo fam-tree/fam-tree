471d327b4216c473888a07ad8973c4c39f68a8c8
Marti <44981000+marti-toonen@users.noreply.github.com>
2024-05-05 02:27:50 -0400

    4th kermit

--- site.js
+++ site.js
@@ -110,6 +110,168 @@ function toggleMenu(id) {
         children.innerHTML = "";
         job.innerHTML = "";
         break;
+      case 'hermina':
+        name.innerHTML = "Hermina Reyen (née Toonen)";
+        birthday.innerHTML = "September 20th, 1809";
+        death.innerHTML = " - ???";
+        parents.innerHTML = "Parents: Petrus & Hendrina";
+        partner.innerHTML = "Partner: Egbertus Reyen";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'mathias':
+        name.innerHTML = "Mathias Toonen";
+        birthday.innerHTML = "December 7th, 1810";
+        death.innerHTML = " - May 7th, 1882";
+        parents.innerHTML = "Parents: Petrus & Hendrina";
+        partner.innerHTML = "Partner: Gertruda Toonen (née Willems)";
+        children.innerHTML = "Children: Gerardus";
+        job.innerHTML = "";
+        break;
+      case 'gertruda1':
+        name.innerHTML = "Gertruda Toonen (née Willems)";
+        birthday.innerHTML = "August 8, 1813";
+        death.innerHTML = " - March 23rd, 1879";
+        parents.innerHTML = "";
+        partner.innerHTML = "Partner: Mathias Toonen";
+        children.innerHTML = "Children: Gerardus";
+        job.innerHTML = "";
+        break;
+      case 'gertruy':
+        name.innerHTML = "Gertruy Toonen";
+        birthday.innerHTML = "March 29th, 1812 - February 9th, 1813";
+        death.innerHTML = " - ???";
+        parents.innerHTML = "Parents: Petrus & Hendrina";
+        partner.innerHTML = "";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'jan':
+        name.innerHTML = "Jan Toonen";
+        birthday.innerHTML = "September 29th, 1813";
+        death.innerHTML = " - October 21st, 1891";
+        parents.innerHTML = "Parents: Petrus & Hendrina";
+        partner.innerHTML = "Partner: Elisabeth Toonen (née Gerits) April 13th, 1822 - July 2nd, 1917";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'elisabeth':
+        name.innerHTML = "Elisabeth Beelen (née Toonen)";
+        birthday.innerHTML = "November 8th, 1817";
+        death.innerHTML = " - December 24th, 1817";
+        parents.innerHTML = "Parents: Petrus & Hendrina";
+        partner.innerHTML = "Partner: Martinus Beelen";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'peter':
+        name.innerHTML = "Peter Toonen";
+        birthday.innerHTML = "November 10th, 1820";
+        death.innerHTML = " - March 10th, 1848";
+        parents.innerHTML = "Parents: Petrus & Hendrina";
+        partner.innerHTML = "";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'hendricus':
+        name.innerHTML = "Hendricus Toonen";
+        birthday.innerHTML = "February 9th, 1823";
+        death.innerHTML = " - ???";
+        parents.innerHTML = "Parents: Petrus & Hendrina";
+        partner.innerHTML = "Partner: Egberdina Toonen (née Arts) November 30th, 1835 - April 25th, 1913";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'arnoldus':
+        name.innerHTML = "Arnoldus Toonen";
+        birthday.innerHTML = "February 9th, 1823";
+        death.innerHTML = " - January 1st, 1886";
+        parents.innerHTML = "Parents: Petrus & Hendrina";
+        partner.innerHTML = "Partner: Maria Toonen (née van den Oever)";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'petronella':
+        name.innerHTML = "Petronella Toonen";
+        birthday.innerHTML = "October 2nd, 1826";
+        death.innerHTML = " - May 23rd, 1844";
+        parents.innerHTML = "Parents: Petrus & Hendrina";
+        partner.innerHTML = "";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'gerardus':
+        name.innerHTML = "Gerardus Toonen";
+        birthday.innerHTML = "August 16th, 1946";
+        death.innerHTML = " - March 6th, 1929";
+        parents.innerHTML = "Parents: Mathias & Gertruda";
+        partner.innerHTML = "Partner: Maria Anna Toonen (née Schraven)";
+        children.innerHTML = "Children: Gertruda, Henricus, Mathijs, Mathijs, Martinus";
+        job.innerHTML = "";
+        break;
+      case 'maria':
+        name.innerHTML = "Maria Anna Toonen (née Schraven)";
+        birthday.innerHTML = "July 27th, 1850";
+        death.innerHTML = " - ???";
+        parents.innerHTML = "";
+        partner.innerHTML = "Partner: Gerardus Toonen";
+        children.innerHTML = "Children: Gertruda, Henricus, Mathijs, Mathijs, Martinus";
+        job.innerHTML = "";
+        break;
+      case 'gertruda2':
+        name.innerHTML = "Gertruda Toonen";
+        birthday.innerHTML = "March 19th, 1878";
+        death.innerHTML = " - ???";
+        parents.innerHTML = "Parents: Gerardus & Maria";
+        partner.innerHTML = "";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'henricus':
+        name.innerHTML = "Henricus Toonen";
+        birthday.innerHTML = "June 11th, 1879";
+        death.innerHTML = " - December 22nd, 1886";
+        parents.innerHTML = "Parents: Gerardus & Maria";
+        partner.innerHTML = "";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'mathijs1':
+        name.innerHTML = "Mathijs Toonen";
+        birthday.innerHTML = "March 22nd, 1881";
+        death.innerHTML = " - March 24th, 1881";
+        parents.innerHTML = "Parents: Gerardus & Maria";
+        partner.innerHTML = "";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'mathijs2':
+        name.innerHTML = "Mathijs Toonen";
+        birthday.innerHTML = "April 20th, 1882";
+        death.innerHTML = " - September 9th, 1958";
+        parents.innerHTML = "Parents: Gerardus & Maria";
+        partner.innerHTML = "Partner: Gerdina Toonen (née Roeffen) February 23rd, 1883 - February 19th, 1956";
+        children.innerHTML = "";
+        job.innerHTML = "";
+        break;
+      case 'martinus':
+        name.innerHTML = "Martinus Toonen";
+        birthday.innerHTML = "June 23rd, 1884";
+        death.innerHTML = " - February 19th, 1956";
+        parents.innerHTML = "Parents: Gerardus & Maria";
+        partner.innerHTML = "Partner: Antonia Toonen (née Jakobs)";
+        children.innerHTML = "Children: Mietja, Nelly, Gerrit, Sjef, Thijs, Johannes, Martien, Truus";
+        job.innerHTML = "";
+        break;
+      case 'antonia3':
+        name.innerHTML = "Antonia Toonen (née Jakobs)";
+        birthday.innerHTML = "October 6th, 1895";
+        death.innerHTML = " - October 18th, 1970";
+        parents.innerHTML = "";
+        partner.innerHTML = "Partner: Martinus Toonen";
+        children.innerHTML = "Children: Mietja, Nelly, Gerrit, Sjef, Thijs, Johannes, Martien, Truus";
+        job.innerHTML = "";
+        break;
     case null:
       alert('tell marti it said null and who you clicked on');
       break;
